21c59ff729cf00a693cb6a1791cf83b144c31309
CorbinSpence <corbinspence16@gmail.com>
2023-04-15 15:51:57 -0400

    Basic demo seed for database

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE employee_db;
+
+INSERT INTO department (name)
+VALUES ("Main");
+
+INSERT INTO role (title, salary)
+VALUES ("Manager", 100000.00);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("David", "Johnson");
+
