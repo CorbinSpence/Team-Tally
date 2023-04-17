337097b2f2981c23dfe8ad87e8098a95fa15a373
CorbinSpence <corbinspence16@gmail.com>
2023-04-17 15:37:56 -0400

    added prompt and db query (bugged)

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
 const inquirer = require('inquirer')
 const mysql = require('mysql2')
 
+const mainMenuChoices = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+
 //TODO: set connection variables
-mysql.createConnection({
+const dbConnection = mysql.createConnection({
     host: 'localhost',
     user: '',
-    password: ''
+    password: '',
+    database: 'employee_db'
 })
 
-inquierer.prompt([
-    {
-        type:'',
-        name:'',
-        message:''
-    }
-]).then((data)=>{
+var stillUsed = true
+const data = inquirer.prompt([
+            {
+                type:'list',
+                name:'dataQuery',
+                message:'What do you what to do?',
+                choices:[...mainMenuChoices],
+            },
+            // {
+            //     type:'text',
+            //     name: 'test',
+            //     message: 'Does This message show?'
+            // }
+        ]).then((data)=>{
+            switch(data.dataQuery){
+                case 'View All Employees':
+                    dbConnection.query(
+                        'SELECT * FROM employee',
+                        function(err, results, field){
+                            console.log(results)
+                        }
+                    )
+                    break;
+                case 'Add Employee':
+                    break;
+                case 'Update Employee Role':
+                    break;
+                case 'View All Roles':
+                    break;
+                case 'Add Role':
+                    break;
+                case 'View All Departments':
+                    break;
+                case 'Add Department':
+                    break;
+                case 'Quit':
+                    stillUsed = false
+                    break;
+            }
+        })
 
-})
+
+
