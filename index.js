cd55dc6ad1447c606e5630c50ac4ee4688721b17
CorbinSpence <corbinspence16@gmail.com>
2023-04-18 17:29:33 -0400

    add functional queries

--- index.js
+++ index.js
@@ -3,9 +3,165 @@ const mysql = require('mysql2')
 
 const mainMenuChoices = ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
 
+function addEmployee(){
+    inquirer.prompt([
+        {
+            type:'text',
+            name:'firstName',
+            message:"What is the employee's first name?"
+        },
+        {
+            type:'text',
+            name:'lastName',
+            message:"What is the employee's last name?"
+        }
+    ]).then((innerData)=>{
+        dbConnection.query(
+            `SELECT id, title FROM role`,
+            (err, results, fields)=>{
+                let titleArr = results.map((val)=>val.title)
+                let roleDict = {}
+                results.forEach((val)=>roleDict[val.title]=val.id)
+                inquirer.prompt([
+                    {
+                        type:'list',
+                        name:'role',
+                        message:"What is the employee's role?",
+                        choices:[...titleArr]
+                    }
+                ]).then((roleData)=>{
+                    dbConnection.query(
+                        `SELECT first_name, last_name, id FROM employee`,
+                        (err, results, fields)=>{
+                            let nameArr = results.map((val)=>val.first_name+' '+val.last_name)
+                            let employeeDict = {}
+                            results.forEach((val)=>employeeDict[val.first_name+' '+val.last_name] = val.id)
+                            inquirer.prompt([
+                                {
+                                    type:'list',
+                                    name:'manager',
+                                    message:"Who is the employee's Manager?",
+                                    choices:[...nameArr]
+                                }
+                            ]).then((employeeData)=>{
+                                dbConnection.query(
+                                    `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${innerData.firstName}", "${innerData.lastName}", ${roleDict[roleData.role]}, ${employeeDict[employeeData.manager]})`,
+                                    function(err, results, field){
+                                        console.log(results)
+                                        mainMenu()
+                                    }
+                                )
+                            })
+                        }
+                    )
+                })
+            }
+        )
+    })
+}
+function addRole(){
+    inquirer.prompt([
+        {
+            type:'text',
+            name:'title',
+            message:'What is the name of this role?'
+        },
+        {
+            type:'number',
+            name:'salary',
+            message:'What is the salary of the role?'
+        }
+    ]).then((data)=>{
+        dbConnection.query(
+            'SELECT * FROM department',
+            function(err, results, field){
+                let dict = {}
+                results.forEach(val=>{dict[val.name] = val.id})
+                let departmentArr = results.map((val)=>{return val.name})
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'department',
+                        message: 'Which department is it from?',
+                        choices: [...departmentArr]
+                    }
+                ]).then((departments)=>{
+                    dbConnection.query(
+                        `INSERT INTO role (title, salary, department_id) VALUES ("${data.title}", ${data.salary}, ${dict[departments.department]})`,
+                        function(err, results, field){
+                            console.log(results)
+                            mainMenu()
+                        }
+                    )
+                })
+            }
+        )
+    })
+}
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type:'text',
+            name:'name',
+            message:'What is the name of this departemnt?'
+        }
+    ]).then((data)=>{
+        dbConnection.query(
+            `INSERT INTO department (name) VALUES ("${data.name}")`,
+            function(err, results, field){
+                console.log(results)
+                mainMenu()
+            }
+        )
+    })
+}
+function updateEmployee(){
+    dbConnection.query(
+        `SELECT first_name, last_name, id FROM employee`,
+        (err, results, fields)=>{
+            let nameArr = results.map((val)=>val.first_name+' '+val.last_name)
+            let employeeDict = {}
+            results.forEach((val)=>employeeDict[val.first_name+' '+val.last_name] = val.id)
+            inquirer.prompt([
+                {
+                    type:'list',
+                    name:'employee',
+                    message:"Which employee's role do you want to update",
+                    choices:[...nameArr]
+                }
+            ]).then((employeeData)=>{
+                dbConnection.query(
+                    `SELECT title, id FROM role`,
+                    (err, results, fields)=>{
+                        let titleArr = results.map((val)=>val.title)
+                        let roleDict = {}
+                        results.forEach((val)=>roleDict[val.title] = val.id)
+                        inquirer.prompt([
+                            {
+                                type:'list',
+                                name:'newRole',
+                                message:"Which role do you want to assign the new employee?",
+                                choices:[...titleArr]
+                            }
+                        ]).then((roleData)=>{
+                            dbConnection.query(
+                                `UPDATE employee SET role_id=${roleDict[roleData.newRole]} WHERE id=${employeeDict[employeeData.employee]}`,
+                                function(err, results, field){
+                                    console.log(results)
+                                    mainMenu()
+                                }
+                            )
+                        })
+                    }
+                )
+            })
+        }
+    )
+    
+    
+}
+
 //TODO: set connection variables
-
-
 const dbConnection = mysql.createConnection({
     host: 'localhost',
     user: '',
@@ -20,6 +176,7 @@ dbConnection.connect(function (err){
     }
     mainMenu()
 })
+
 function mainMenu(){
     inquirer.prompt([
         {
@@ -28,11 +185,6 @@ function mainMenu(){
             message:'What do you what to do?',
             choices:[...mainMenuChoices],
         },
-        // {
-        //     type:'text',
-        //     name: 'test',
-        //     message: 'Does This message show?'
-        // }
     ]).then((data)=>{
         switch(data.dataQuery){
             case 'View All Employees':
@@ -45,24 +197,10 @@ function mainMenu(){
                 )
                 break;
             case 'Add Employee':
-                //TODO: add prompted data to query statement
-                dbConnection.query(
-                    'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ()',
-                    function(err, results, field){
-                        console.log(results)
-                        mainMenu()
-                    }
-                )
+                addEmployee()
                 break;
             case 'Update Employee Role':
-                //TODO: add prompted data to query statement
-                dbConnection.query(
-                    'UPDATE employee SET ${columns} WHERE ${condition}',
-                    function(err, results, field){
-                        console.log(results)
-                        mainMenu()
-                    }
-                )
+                updateEmployee()
                 break;
             case 'View All Roles':
                 dbConnection.query(
@@ -74,14 +212,7 @@ function mainMenu(){
                 )
                 break;
             case 'Add Role':
-                //TODO: add prompted data to query statement
-                dbConnection.query(
-                    'INSERT INTO role (title, salary, department_id) VALUES ()',
-                    function(err, results, field){
-                        console.log(results)
-                        mainMenu()
-                    }
-                )
+                addRole()
                 break;
             case 'View All Departments':
                 dbConnection.query(
@@ -93,14 +224,7 @@ function mainMenu(){
                 )
                 break;
             case 'Add Department':
-                //TODO: add prompted data to query statement
-                dbConnection.query(
-                    'INSERT INTO department (name) VALUES ()',
-                    function(err, results, field){
-                        console.log(results)
-                        mainMenu()
-                    }
-                )
+                addDepartment()
                 break;
             case 'Quit':
                 process.exit(0)
